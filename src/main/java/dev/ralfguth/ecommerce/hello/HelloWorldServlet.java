90b31f2dddb9aadebddd043c5f84a67395c702ca
Ralf Guth <rsguth@gmail.com>
2024-06-27 20:48:10 -0300

    chore: configuracao inicial do projeto maven

---
+++ src/main/java/dev/ralfguth/ecommerce/hello/HelloWorldServlet.java
@@ -0,0 +1,20 @@
+package dev.ralfguth.ecommerce.hello;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet {
+    
+	private static final long serialVersionUID = 1L;
+
+	@Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Hello, World!");
+    }
+}
+
